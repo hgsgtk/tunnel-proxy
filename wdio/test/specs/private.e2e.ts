1c9c367d5aaee87948d7d3d25e5775da56e866e3
hgsgtk <hgsgtk@gmail.com>
2021-08-25 07:27:05 +0900

    test: put tunnel client and sever in same network
    I found the error gotten by wdio test.
    
    previous:
    
    ```
    [chrome 92.0.4515.131 linux #0-0] unknown error: net::ERR_NAME_NOT_RESOLVED
      (Session info: headless chrome=92.0.4515.131)
    ```
    
    now:
    
    ```
    [chrome 92.0.4515.131 linux #0-0] unknown error: net::ERR_CONNECTION_REFUSED
      (Session info: headless chrome=92.0.4515.131)
    ```

--- wdio/test/specs/private.e2e.ts
+++ wdio/test/specs/private.e2e.ts
@@ -1,6 +1,7 @@
 describe('example.com', () => {
     it('should open private example.com in different network', async () => {
-        const url = 'http://sut-in-private:8080/example'
+        // const url = 'http://sut-in-private:8080/example' want to request sut-in-private:8080
+        const url = 'http://wstunnel-in-cloud:3331/example'
         await browser.url(url)
         const actual = await browser.getUrl()
         await expect(actual).toEqual(url)
